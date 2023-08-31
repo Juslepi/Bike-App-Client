ec6a07c9c044ab79ba5eeeaaa9b3e5d9a6dc91f0
unknown <juslepi@gmail.com>
2023-08-31 12:34:27 +0300

    Fetch data

--- src/pages/Journeys.tsx
+++ src/pages/Journeys.tsx
@@ -1,9 +1,20 @@
-import { useEffect } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const Journeys = () => {
+  const [journeys, setJourneys] = useState<any>([])
+
+  useEffect(() => {
+    const getJourneys = async () => {
+      const res = await axios.get("http://localhost:3001/api/journeys")
+      setJourneys(res.data)
+    }
+    getJourneys()
+  }, [])
+
   return (
     <div>
-      <p>Journey list</p>
+
     </div>
   )
 }
