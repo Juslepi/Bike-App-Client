bd1cfe5f4ab9896f6e388f20242e66c805872980
unknown <juslepi@gmail.com>
2023-09-01 09:55:50 +0300

    Add stations table

--- src/pages/Stations.tsx
+++ src/pages/Stations.tsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+import { DataGrid } from '@mui/x-data-grid';
+import { Station } from "../types/types"
+import axios from "axios"
+
+const cols = [
+  { field: 'Name', headerName: 'Name', width: 150 },
+  { field: 'Address', headerName: 'Address', width: 150 },
+  { field: 'Capacity', headerName: 'Capacity', width: 100 },
+]
+
 const Stations = () => {
+  const [stations, setStations] = useState<Station[]>([])
+
+  useEffect(() => {
+    const getStations = async () => {
+      const res = await axios.get("http://localhost:3001/api/stations")
+      setStations(res.data)
+    }
+
+    getStations()
+  }, [])
+
   return (
     <div>
-        <p>Stations list</p>
+      <DataGrid getRowId={(row) => row._id} rows={stations} columns={cols} />
     </div>
   )
 }
